be16533daa304e0b53e32621a290b7ad5e0501d6
Henry Ripple <hripple46@gmail.com>
2023-06-11 13:43:05 -0500

    if user has name, name is shown, else username shown

--- models/user.js
+++ models/user.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   firstName: String,
   lastName: String,
-  username: String,
-  memberStatus: String,
-  password: String,
+  username: { type: String, required: true },
+  memberStatus: { type: String, default: "Basic" },
+  password: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
